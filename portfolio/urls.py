81d5a8b2ddedee2032dc6f58ab020b2f98bb356d
Diegoaqg <tigidmusic@gmail.com>
2025-07-12 15:36:19 -0500

    12 de julio django

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('projects/', views.projects, name='projects'),
     path('aboutme/', views.aboutme, name='aboutme'),
+    path('proyectos/', views.listaProyectos,name='listaProyectos'),
+    path('proyectos/crear/', views.crear_proyectos, name='crear_proyecto'),
+    path('proyectos/editar/<int:pk>/', views.editar_proyecto, name='editar_proyecto'),
+    path('proyectos/eliminar/<int:pk>/', views.eliminar_proyecto, name='eliminar_proyecto'),
+
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Proyecto
+from .forms import ProyectoForm
 
 def home(request):
     return render(request, 'portfolio/home.html')
@@ -11,3 +13,33 @@ def contact(request):
 
 def aboutme(request):
     return render(request, 'portfolio/aboutme.html')
+
+def listaProyectos(request):
+    Proyectos = Proyecto.objects.all()
+    return render(request, 'portfolio/listaproyectos.html', {'proyectos':Proyectos})
+
+def crear_proyectos(request):
+    if request.method == 'POST':
+        form =ProyectoForm(request-POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listaProyectos')
+    else:
+        form = ProyectoForm()
+    return render(request, 'portfolio/formulario.html', {'form' : form})
+
+def editar_proyecto(request, pk):
+    proyecto = get_object_or_404(Proyecto, pk=pk)
+    if request.method == 'POST':
+        proyecto.delete()
+        return redirect('listarProyectos')
+    return render(request, 'portfolio/confirmar_eliminar.html', {'proyecto' : proyecto})
+
+def eliminar_proyecto(request, pk):
+    proyecto = get_object_or_404(Proyecto, pk=pk)
+    
+    if request.method == 'POST':
+        proyecto.delete()
+        return redirect('listaProyectos')  
+
+    return render(request, 'portfolio/confirmar_eliminar.html', {'proyecto': proyecto})
